01b119a65554e624d20e855110dd8c018d012d42
Troy Danka <troydanka@gmail.com>
2016-02-21 13:06:26 -0500

    Updated config/routs.rb with home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  #get 'pages/home'
+  root "pages#home"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+#Rails.root:/Users/troydanka/Desktop/pinteresting
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
